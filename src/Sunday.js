fe33825dde430725edc55ca1781de11b7fd1d486
Sunattulo <muslimamuminova6969@gmail.com>
2023-01-24 00:23:49 +1200

    a

---
+++ src/Sunday.js
@@ -0,0 +1,13 @@
+const Sunday = () => {
+  return (
+    <div className="tex">
+      <ul>
+        <li>html0</li>
+        <li>html1</li>
+        <li>html2</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sunday;
---
+++ src/Texnologi.js
@@ -0,0 +1,13 @@
+const Texnologi = () => {
+  return (
+    <div className="tex">
+      <ul>
+        <li>css0</li>
+        <li>css1</li>
+        <li>css2</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Texnologi;
